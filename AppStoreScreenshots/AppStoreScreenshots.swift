dae7ca628a350b607a07b4f3b96b70342289a94a
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-05-04 21:57:52 +1000

    Cleanup AppStoreScreenshots.swift

--- AppStoreScreenshots/AppStoreScreenshots.swift
+++ AppStoreScreenshots/AppStoreScreenshots.swift
@@ -1,33 +1,13 @@
-//
-//  AppStoreScreenshots.swift
-//  AppStoreScreenshots
-//
-//  Created by Aleph Retamal on 4/5/20.
-//  Copyright © 2020 Aleph Retamal. All rights reserved.
-//
-
 import XCTest
 
 class AppStoreScreenshots: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
+    override func tearDownWithError() throws {}
 
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
