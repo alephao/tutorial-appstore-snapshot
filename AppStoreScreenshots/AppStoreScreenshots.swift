29f59b2013d472a8aca7657fda318bea29fb941f
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-05-04 22:00:06 +1000

    Override Environment

--- AppStoreScreenshots/AppStoreScreenshots.swift
+++ AppStoreScreenshots/AppStoreScreenshots.swift
@@ -5,14 +5,34 @@ import SwiftUI // Access to UIHostingController
 
 class AppStoreScreenshots: XCTestCase {
 
-    override func setUpWithError() throws {}
-    override func tearDownWithError() throws {}
+  override func setUpWithError() throws {}
+  override func tearDownWithError() throws {}
 
-    func testScreenshots() throws {
-      record = true // 1
-      let view = GreetingView() // 2
-      let vc = UIHostingController(rootView: view) // 3
-      assertSnapshot(matching: vc, as: .image(on: .iPhoneXsMax)) // 4
+  func withEnvironment(
+    _ env: MyAwesomeAppFramework.Environment,
+    body: @escaping () -> Void) {
+    let oldEnv = AppEnvironment.shared.env
+    AppEnvironment.shared.env = env
+    body()
+    AppEnvironment.shared.env = oldEnv
+  }
+
+  func withEnvironment(
+    userDefaults: UserDefaults = AppEnvironment.shared.userDefaults,
+    language: Language = AppEnvironment.shared.language,
+    body: @escaping () -> Void) {
+    let env = Environment(userDefaults: userDefaults, language: language)
+    withEnvironment(env, body: body)
+  }
+
+  func testScreenshots() throws {
+    record = true
+    let userDefaults = UserDefaults() // 1
+    withEnvironment(userDefaults: userDefaults, language: .enUS) {
+      let view = GreetingView()
+      let vc = UIHostingController(rootView: view)
+      assertSnapshot(matching: vc, as: .image(on: .iPhoneXsMax))
     }
+  }
 
 }
